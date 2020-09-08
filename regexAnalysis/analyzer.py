2d89612f6117c856dd0eb7f3cdc6eeafc2bea05f
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-09-09 07:33:19 +1200

    Update analyzer.py

--- regexAnalysis/analyzer.py
+++ regexAnalysis/analyzer.py
@@ -1,3 +1,5 @@
+import re
+
 js_file = "class Rectangle {constructor(height, width) {this.height = height;\
                this.width = width;} get area(){return this.calcArea();}   calcArea()\
                {return this.height * this.width; }}const square = new Rectangle\
