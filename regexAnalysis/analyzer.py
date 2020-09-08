f6b20156d22fd6dde2b2dddea5617ab1b23c7a20
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-09-09 07:32:38 +1200

    Create analyzer.py

---
+++ regexAnalysis/analyzer.py
@@ -0,0 +1,7 @@
+js_file = "class Rectangle {constructor(height, width) {this.height = height;\
+               this.width = width;} get area(){return this.calcArea();}   calcArea()\
+               {return this.height * this.width; }}const square = new Rectangle\
+               (10, 10);console.log(square.area);"
+print(js_file)
+class_name_finder = re.search("class: (\w+)", js_file)
+print(class_name_finder.groups())
