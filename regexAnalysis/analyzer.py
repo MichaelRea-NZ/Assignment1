3e2c22fab6d2db56f8f59c0f6f1fbe3807541201
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-09-11 07:03:59 +1200

    Update analyzer.py

--- regexAnalysis/analyzer.py
+++ regexAnalysis/analyzer.py
@@ -5,5 +5,13 @@ js_file = "class Rectangle {constructor(height, width) {this.height = height;\
                {return this.height * this.width; }}const square = new Rectangle\
                (10, 10);console.log(square.area);"
 print(js_file)
-class_name_finder = re.search("class: (\w+)", js_file)
-print(class_name_finder.groups())
+#class_name_finder = re.search("class: (\w+)", js_file)
+class_name_finder = re.match(r'^class.(\w+)', js_file, re.X)
+#print(class_name_finder.groups())
+
+if class_name_finder is not None:
+   # print(class_name_finder.group())
+    #print(class_name_finder.group().split())
+    print(class_name_finder.group().split()[-1])
+    class_name = class_name_finder.group().split()[-1]
+    print(class_name)
