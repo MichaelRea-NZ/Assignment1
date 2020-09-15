47eb3141d877a5e9761497304af3f49df00c7a31
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-09-15 12:19:12 +1200

    Update analyzer2.py

--- regexAnalysis/analyzer2.py
+++ regexAnalysis/analyzer2.py
@@ -3,17 +3,15 @@ import re
 
 class Analyzer:
     def __init__(self):
-        self.js_file = "class Rectangle {constructor(height, width) {this.height = height;\
+        #TODO make up scripts to test function declerations
+        self.js_file = "function Rectangle {constructor(height, width) {this.height = height;\
                this.width = width;} get area(){return this.calcArea();}  \
                calcArea() {return this.height * this.width; }}const square = \
-               new Rectangle (10, 10);console.log(square.area);"
-        self.function_list = []
+               function new Rectangle (10, 10);console.log(square.area);"
 
     def find_class(self):
-        #print(self.js_file)
         class_name_finder = re.match(r'^class.(\w+)', self.js_file)
         if class_name_finder is not None:
-            #print(class_name_finder.group().split()[-1] + '\n')
             class_name = class_name_finder.group().split()[-1]
             print(class_name)
             #self.find_class(class_name_finder, self.js_file)
@@ -29,35 +27,30 @@ class Analyzer:
 #
 #         find_class(class_name_finder, js_file)
 
-
-function_list = []
-
-
-def find_function_1(js_file):
-    func_declaration_1 = re.findall(r'^function.(\w+)', js_file)
-    if find_function_1 is not None:
-        function_list.append(func_declaration_1.group().split()[-1])
-        print(function_list)
+    def find_function_1(self):
+        #TODO make findall() read though multipile lines
+        func_declaration_1 = re.findall(r'^function.(\w+)',self.js_file)
+        if func_declaration_1 is not None:
+            function_name = func_declaration_1
+            #function_list.append(func_declaration_1.group().split()[-1])
+            print(function_name)
 
 
-def find_function_2(func, js_file):
-    func_declaration_2 = re.findall(r'\w+ ?=[=\\.+)=>]', js_file)
-    if find_function_2 is not None:
-        function_list.append(func_declaration_2.group().split()[-1])
-        print(function_list)
+    def find_function_2(self):
+        func_declaration_2 = re.findall(r'\w+ ?=[=\\.+)=>]', self.js_file)
+        if func_declaration_2 is not None:
+            function_name =func_declaration_2
+            print(function_name)
 
 
 
 """func_declaration_1 = re.findall(r'^function.(\w+)', js_file)
 func_declaration_2 = re.findall(r' \w+ ?=[= (.+) =>]', js_file)
-
 def find_function(func, js_file):
     if find_function is not None:
         function_list.append(func.group().split()[-1])
     else:
         print("no functions found")
 print(function_list)
-
-
 find_function(func_declaration_1, js_file)
 find_function(func_declaration_2, js_file)"""
