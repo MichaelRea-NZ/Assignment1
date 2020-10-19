9e48ba2151fc04222dd5c8590cc777e5b857bb79
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-10-19 13:22:35 +1300

    Update analyzer.py

--- regexAnalysis/analyzer.py
+++ regexAnalysis/analyzer.py
@@ -3,42 +3,53 @@ import re
 
 class Analyzer:
     def __init__(self, file_content):
-        # self.js_file = " class Rectangle constructor (height, width) {this.height = height;\
-        #     this.width = width;} get class area(){return this.calcArea();}  \
-        #     this.speed = speed;        this.name = name; calcArea() \
-        #     { function return this.height * this.width; }} = const square\
-        #     function new Rectangle (10, 10);console.log(square.area);"
         self.js_file = file_content
 
     def find_class(self):
         global class_name
-        class_name = re.search(r'class.(\w+)', self.js_file)
-        if class_name is not None:
-            class_name = class_name.group().split()[-1]
-            print(class_name)
-            """global classList
-            classList = [class_name]
-            print(classList)"""
+        try:
+            class_name = re.search(r'class.(\w+)', self.js_file)
+            if self.js_file == '':
+                raise Exception
+        except Exception as e:
+            print('A JS file needs to be selected to run the analyzer')
+        else:
+            if class_name is not None:
+                class_name = class_name.group().split()[-1]
+                print(class_name)
 
     def find_property(self):
         global property_name
-        property_name = re.findall(r'this.(\w+)', self.js_file)
-        if property_name is not None:
-            first_list = property_name
-            print(first_list)
-            property_name = list(dict.fromkeys(first_list))
-            property_name = (['{}\l'.format(i) for i in property_name])
-            property_name = " ".join(property_name)
-            print(property_name)
+        try:
+            property_name = re.findall(r'this.(\w+)', self.js_file)
+            if self.js_file == '':
+                raise Exception
+        except Exception as e:
+            print('A JS file needs to be selected to run the analyzer')
+        else:
+            if property_name is not None:
+                first_list = property_name
+                print(first_list)
+                first_list = [i.lower() for i in first_list]
+                property_name = list(dict.fromkeys(first_list))
+                property_name = (['{}\l'.format(i) for i in property_name])
+                property_name = " ".join(property_name)
+                print(property_name)
 
     def find_function_1(self):
         global function_name
-        function_name = re.findall(r'function.(\w+)', self.js_file)
-        if function_name is not None:
-            function_name = function_name
-            function_name = (['{}()\l'.format(i) for i in function_name])
-            function_name = " ".join((function_name))
-            print(function_name)
+        try:
+            function_name = re.findall(r'function.(\w+)', self.js_file)
+            if self.js_file == '':
+                raise Exception
+        except Exception as e:
+            print('A JS file needs to be selected to run the analyzer')
+        else:
+            if function_name is not None:
+                function_name = function_name
+                function_name = (['{}()\l'.format(i) for i in function_name])
+                function_name = " ".join((function_name))
+                print(function_name)
 
     def create_file(self):
         dot_file1 = open("classfile.dot", "w")
